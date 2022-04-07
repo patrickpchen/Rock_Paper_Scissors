e903fbdfc8427f7e710b4fa7aa06218eec719e2a
Pin-Hsi Patrick Chen <patrick.tucson@gmail.com>
2022-04-07 18:39:47 +0800

    Delete unnecessary lines of code

--- script.js
+++ script.js
@@ -60,10 +60,6 @@ function playRound(playerSelection, computerSelection) {
     } 
   }
 
-  //const playerSelection = "rock";
-  //const computerSelection = computerPlay();
-  //console.log(playRound(playerSelection, computerSelection));
-
 //Create a function for five rounds of the game.
 function game(){
     let playerWins = 0;
